a4e78cffcbc972fa647bc4e31cf583b254b5087e
realrgt <ergitorubildo@gmail.com>
2021-06-04 12:13:16 +0200

    external resources -- stormglass client

---
+++ src/clients/__test__/stormGlass.test.ts
@@ -0,0 +1,21 @@
+import { StormGlass } from '@src/clients/stormGlass';
+import axios from 'axios';
+
+import stormGlassWeather3HoursFixture from '@test/fixtures/stormglass_weather_3_hours.json';
+import stormGlassNormalized3HoursFixture from '@test/fixtures/stormglass_normalized_response_3_hours.json';
+
+jest.mock('axios');
+
+describe('StormGlass client', () => {
+  it('Should return normalized forecast from the stormGlass service', async () => {
+    const lat = 1;
+    const lng = 2;
+
+    axios.get = jest.fn().mockReturnValue(stormGlassWeather3HoursFixture);
+
+    const stormGlass = new StormGlass(axios);
+    const response = await stormGlass.fetchPoints(lat, lng);
+
+    expect(response).toEqual(stormGlassNormalized3HoursFixture);
+  });
+});
---
+++ src/clients/stormGlass.ts
@@ -0,0 +1,15 @@
+import { AxiosStatic } from 'axios';
+
+export class StormGlass {
+  readonly stormGlassAPIParams =
+    'swellDirection,swellHeight,swellPeriod,waveDirection,waveHeight,windDirection,windSpeed';
+  readonly stormGlassAPISource = 'noaa';
+
+  constructor(protected request: AxiosStatic) {}
+
+  public fetchPoints(lat: number, lng: number): Promise<Record<string, never>> {
+    return this.request.get(
+      `https://api.stormglass.io/v2/weather/point?params=${this.stormGlassAPIParams}&source=${this.stormGlassAPISource}&lat=${lat}&lng=${lng}`
+    );
+  }
+}
