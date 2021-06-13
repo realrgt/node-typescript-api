1d9208bd4a97c4879db230bb9c1521130d19ca47
realrgt <ergitorubildo@gmail.com>
2021-06-13 21:15:08 +0200

    refactor forecast service test -- proper way of mocking stormglass dependecy

--- src/services/__test__/forecast.test.ts
+++ src/services/__test__/forecast.test.ts
@@ -5,10 +5,13 @@ import { Beach, BeachPosition, Forecast } from '../forecast';
 jest.mock('@src/clients/stormGlass');
 
 describe('Forecast Service', () => {
+  //
+  const mockedStormGlassService = new StormGlass() as jest.Mocked<StormGlass>;
+
   test('should return the forecast for a list of beaches', async () => {
-    StormGlass.prototype.fetchPoints = jest
-      .fn()
-      .mockResolvedValue(stormGlassNormalizedResponseFixture);
+    mockedStormGlassService.fetchPoints.mockResolvedValue(
+      stormGlassNormalizedResponseFixture
+    );
 
     const beaches: Beach[] = [
       {
@@ -83,7 +86,7 @@ describe('Forecast Service', () => {
       },
     ];
 
-    const forecast = new Forecast(new StormGlass());
+    const forecast = new Forecast(mockedStormGlassService);
     const beachesWithRating = await forecast.processForecastForBeaches(beaches);
     expect(beachesWithRating).toEqual(expectedResponse);
   });
