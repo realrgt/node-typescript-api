1039089faba5e739d983ed92b6dd2830374797d5
realrgt <ergitorubildo@gmail.com>
2021-06-13 20:47:13 +0200

    forecast service -- map forecasts by time

--- src/services/forecast.ts
+++ src/services/forecast.ts
@@ -15,22 +15,25 @@ export interface Beach {
   user: string;
 }
 
-export interface BeachForecast extends Omit<Beach, 'user'>, ForecastPoint {
-  rating: number;
-}
+export interface BeachForecast extends Omit<Beach, 'user'>, ForecastPoint {}
 
+export interface TimeForecast {
+  time: string;
+  forecast: BeachForecast[];
+}
 export class Forecast {
   constructor(protected stormGlass = new StormGlass()) {}
 
   public async processForecastForBeaches(
     beaches: Beach[]
-  ): Promise<BeachForecast[]> {
+  ): Promise<TimeForecast[]> {
     const pointsWithCorrectSources: BeachForecast[] = [];
 
     for (const beach of beaches) {
       const points = await this.stormGlass.fetchPoints(beach.lat, beach.lng);
 
       const enrichedBeachData = points.map((e) => ({
+        ...{},
         ...{
           lat: beach.lat,
           lng: beach.lng,
@@ -43,6 +46,25 @@ export class Forecast {
       pointsWithCorrectSources.push(...enrichedBeachData);
     }
 
-    return pointsWithCorrectSources;
+    return this.mapForecastByTime(pointsWithCorrectSources);
+  }
+
+  private mapForecastByTime(forecast: BeachForecast[]): TimeForecast[] {
+    const forecastByTime: TimeForecast[] = [];
+
+    forecast.forEach((point) => {
+      const timePoint = forecastByTime.find((f) => f.time === point.time);
+
+      if (timePoint) {
+        timePoint.forecast.push(point);
+      } else {
+        forecastByTime.push({
+          time: point.time,
+          forecast: [point],
+        });
+      }
+    });
+
+    return forecastByTime;
   }
 }
