6afd99ab2021a604958443b9071827bf47798722
realrgt <ergitorubildo@gmail.com>
2021-06-13 23:08:19 +0200

    setup -- mongo

--- src/server.ts
+++ src/server.ts
@@ -3,15 +3,17 @@ import { Server } from '@overnightjs/core';
 import bodyParser from 'body-parser';
 import { ForecastController } from './controllers/forecast';
 import { Application } from 'express';
+import * as database from '@src/database';
 
 export class SetupServer extends Server {
   constructor(private port = 3000) {
     super();
   }
 
-  public init(): void {
+  public async init(): Promise<void> {
     this.setupExpress();
     this.setupControllers();
+    this.setupDatabase();
   }
 
   private setupExpress(): void {
@@ -21,6 +23,14 @@ export class SetupServer extends Server {
   private setupControllers(): void {
     const forecastController = new ForecastController();
     this.addControllers([forecastController]);
+  }
+
+  private async setupDatabase(): Promise<void> {
+    await database.connect();
+  }
+
+  public async close(): Promise<void> {
+    await database.close();
   }
 
   public getApp(): Application {
