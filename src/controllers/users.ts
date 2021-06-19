bbec5954911df26793d890acd2a63511685b7b34
realrgt <ergitorubildo@gmail.com>
2021-06-19 08:48:52 +0200

    user -- validation errors

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,14 +1,20 @@
 import { Controller, Post } from '@overnightjs/core';
 import { User } from '@src/models/user';
 import { Request, Response } from 'express';
+import { BaseController } from '.';
 
 @Controller('users')
-export class UsersController {
+export class UsersController extends BaseController {
   @Post('')
   public async createUser(req: Request, res: Response): Promise<void> {
-    const user = new User(req.body);
-    const newUser = await user.save();
+    try {
+      const user = new User(req.body);
+      const newUser = await user.save();
 
-    res.status(201).send(newUser);
+      res.status(201).send(newUser);
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (error: any) {
+      this.sendCreateUpdateErrorResponse(res, error);
+    }
   }
 }
--- test/functional/users.test.ts
+++ test/functional/users.test.ts
@@ -16,5 +16,21 @@ describe('Users test functional', () => {
       expect(response.status).toBe(201);
       expect(response.body).toEqual(expect.objectContaining(newUser));
     });
+
+    test('should return a 422 when there is a validation error', async () => {
+      const newUser = {
+        // name: 'John Doe', //-- should fail 'cause this is required
+        email: 'john@email.com',
+        password: '123456',
+      };
+
+      const response = await global.testRequest.post('/users').send(newUser);
+
+      expect(response.status).toBe(422);
+      expect(response.body).toEqual({
+        code: 422,
+        error: 'User validation failed: name: Path `name` is required.',
+      });
+    });
   });
 });
